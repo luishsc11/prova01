00bd4d4ff1cd5b19f7350fec1e04af6ed6d13004
luís henrique <103255891+luishsc11@users.noreply.github.com>
2025-03-29 00:20:14 +0000

    prova

---
+++ prova01/components/ScrollScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import FlatListScreen from './scroll/FlatListScreen';
+import FormScreen from './scroll/FormScreen';
+import SectionListScreen from './scroll/SectionListScreen';
+const Tab = createBottomTabNavigator();
+
+
+export default function App() {
+    return (
+      
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === 'Lista') {
+                            iconName = focused
+                                ? 'list'
+                                : 'list-outline';
+                        } else if (route.name === 'Secao') {
+                            iconName = focused
+                                ? 'config'
+                                : 'configuration-outline';
+                        } else if (route.name === 'Formulario') {
+                            iconName = focused
+                                ? 'form'
+                                : 'form-outline';
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
+                <Tab.Screen name="Lista" component={FlatListScreen} />
+                <Tab.Screen name="Secao" component={SectionListScreen} />
+                <Tab.Screen name="Formulario" component={FormScreen} />
+            </Tab.Navigator>
+    );
+}
